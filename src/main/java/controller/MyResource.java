907c52bfbea655e7f22d0752c8d213f330308339
Paulo Gonçalves <paulogoncalves@recife.ifpe.edu.br>
2023-09-19 15:11:21 -0300

    Dividendos

--- src/main/java/controller/MyResource.java
+++ src/main/java/controller/MyResource.java
@@ -6,21 +6,20 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.FormParam;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/stock")
+@Path("stock")
 public class MyResource {
 
     @GET
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Produces(MediaType.TEXT_HTML)
-    public String listEmployees(@FormParam("ticker") String ticker, @FormParam("type") String type) throws IOException, InterruptedException {
-        String url = String.format("https://statusinvest.com.br/%s/%s", type, ticker);
+    public String listEmployees(@QueryParam("ticker") String ticker, @QueryParam("type") String type) throws IOException, InterruptedException {
+        String url = String.format("https://investidor10.com.br/%s/%s", type, ticker);
+        // String url = String.format("https://statusinvest.com.br/%s/%s", type, ticker);
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).GET().build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -1,13 +1,54 @@
+let map = new Map();
+map.set('Preço', '//*[@id="cards-ticker"]/div[1]/div[2]/div/span');
+// map.set('P/L', '//*[@id="table-indicators"]/div[1]/div[1]/span');
+map.set('VPA', '//*[@id="table-indicators"]/div[17]/div[1]/span');
+map.set('LPA', '//*[@id="table-indicators"]/div[18]/div[1]/span');
 async function logMovies(type, ticker) {
     const url = new URL("http://localhost:8080/Stock/webresources/stock");
     url.searchParams.append("ticker", ticker);
     url.searchParams.append("type", type);
-    console.log(url.href);
-
     const response = await fetch(url.href);
     const html = await response.text();
+    let doc = createDoc(html);
+    let thead = document.querySelector("thead");
+    let tr = document.createElement("tr");
+    thead.appendChild(tr);
+    let th = document.createElement("th");
+    tr.appendChild(th);
+    th.textContent = "Ticker";
+    map.forEach((value, key) => {
+        let th = document.createElement("th");
+        tr.appendChild(th);
+        th.textContent = key;
+    });
+    let tbody = document.querySelector("tbody");
+    tr = document.createElement("tr");
+    tbody.appendChild(tr);
+    td = tr.insertCell(-1);
+    td.textContent = ticker.toUpperCase();
+    map.forEach((value, key, map) => {
+        const iterator = doc.evaluate(value, doc, null, XPathResult.ANY_TYPE, null);
+        let element = iterator.iterateNext();
+        let number = element.textContent.replace(/[^0-9,.]/g, '').replace(',', '.');
+        let td = tr.insertCell(-1);
+        td.textContent = number;
+    });
+    const iterator = doc.evaluate('//*[@id="table-dividends-history"]/tbody', doc, null, XPathResult.ANY_TYPE, null);
+    let element = iterator.iterateNext();
+    getMinYearDividends(element);
+}
+function getMinYearDividends(table) {
+    [...table.rows].forEach(tr => {
+        let data = tr.cells[2].textContent;
+        const [day, month, year] = data.split('/');
+        const date = new Date(+year, +month - 1, +day);
+        let divi = tr.cells[3].textContent.replace(',', '.');
+        let dividendos = Number(divi);
+        console.log(date, dividendos);
+    });
+}
+function createDoc(html) {
     let parser = new DOMParser();
-    let doc = parser.parseFromString(html, 'text/html');
-    console.log(html);
+    return parser.parseFromString(html, 'text/html');
 }
 logMovies("acoes", "sanb11");
