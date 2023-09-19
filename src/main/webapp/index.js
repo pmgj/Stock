288b5c459edeaf5cc0935135ad6fdd7f8fa88771
Paulo Gonçalves <paulogoncalves@recife.ifpe.edu.br>
2023-09-19 15:53:33 -0300

    dgksdj

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -1,3 +1,8 @@
+let columns = [
+    { header: 'Preço', xpath: '//*[@id="cards-ticker"]/div[1]/div[2]/div/span', type: 'currency', f: null },
+    { header: 'VPA', xpath: '//*[@id="table-indicators"]/div[17]/div[1]/span', type: 'currency', f: null },
+    { header: 'LPA', xpath: '//*[@id="table-indicators"]/div[18]/div[1]/span', type: 'currency', f: null },
+];
 let map = new Map();
 map.set('Preço', '//*[@id="cards-ticker"]/div[1]/div[2]/div/span');
 // map.set('P/L', '//*[@id="table-indicators"]/div[1]/div[1]/span');
@@ -16,26 +21,33 @@ async function logMovies(type, ticker) {
     let th = document.createElement("th");
     tr.appendChild(th);
     th.textContent = "Ticker";
-    map.forEach((value, key) => {
+    columns.forEach(obj => {
         let th = document.createElement("th");
         tr.appendChild(th);
-        th.textContent = key;
+        th.textContent = obj.header;
     });
+    createColumns(ticker, doc);
+}
+function createColumns(ticker, doc) {
     let tbody = document.querySelector("tbody");
-    tr = document.createElement("tr");
+    let tr = document.createElement("tr");
     tbody.appendChild(tr);
-    td = tr.insertCell(-1);
+    let td = tr.insertCell(-1);
     td.textContent = ticker.toUpperCase();
-    map.forEach((value, key, map) => {
-        const iterator = doc.evaluate(value, doc, null, XPathResult.ANY_TYPE, null);
-        let element = iterator.iterateNext();
-        let number = element.textContent.replace(/[^0-9,.]/g, '').replace(',', '.');
-        let td = tr.insertCell(-1);
-        td.textContent = number;
+    let formatter = new Intl.NumberFormat('pt-br', { style: 'currency', currency: 'BRL' });
+    columns.forEach(obj => {
+        if (obj.xpath) {
+            const iterator = doc.evaluate(obj.xpath, doc, null, XPathResult.ANY_TYPE, null);
+            let element = iterator.iterateNext();
+            let number = element.textContent.replace(/[^0-9,.]/g, '').replace(',', '.');
+            let td = tr.insertCell(-1);
+            if (obj.type === 'currency') {
+                td.textContent = formatter.format(number);
+            } else {
+                td.textContent = number;
+            }
+        }
     });
-    const iterator = doc.evaluate('//*[@id="table-dividends-history"]/tbody', doc, null, XPathResult.ANY_TYPE, null);
-    let element = iterator.iterateNext();
-    getMinYearDividends(element);
 }
 function getMinYearDividends(table) {
     [...table.rows].forEach(tr => {
