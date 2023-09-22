a27595654df0a967b3a4b0e8cc924bbb7d430b29
Paulo Gonçalves <paulogoncalves@recife.ifpe.edu.br>
2023-09-22 10:11:44 -0300

    Improved dividends code

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -1,14 +1,59 @@
+class Dividends {
+    constructor(name) {
+        this.name = name;
+    }
+    setMatrix(matrix) {
+        this.matrix = matrix;
+    }
+    getDividendsByYear() {
+        let map = new Map();
+        this.matrix.forEach(tr => {
+            let data = tr.payDate;
+            const date = new Date(data);
+            let dividendos = tr.value;
+            let fyear = date.getFullYear();
+            if (map.has(fyear)) {
+                let n = map.get(fyear);
+                n += dividendos;
+                map.set(fyear, n);
+            } else {
+                map.set(fyear, dividendos);
+            }
+        });
+        return map;
+    }
+    toString() {
+        return this.name;
+    }
+}
+class MinFiveYear extends Dividends {
+    constructor() {
+        super("Menor dividendos nos últimos 5 anos");
+    }
+    compute() {
+        let map = this.getDividendsByYear();
+        let today = new Date();
+        let cYear = today.getFullYear();
+        let div = [];
+        for (let index = cYear - 5; index <= cYear - 1; index++) {
+            div.push(map.get(index));
+        }
+        div.sort((a, b) => a - b);
+        return div.shift();
+    }
+}
 let columns = [
     { header: "Ticker", prop: "ticker", type: 'string', f: upperCase },
     { header: 'Preço', prop: "preco", type: 'currency', f: identity },
     { header: 'VPA', prop: "vpa", type: 'currency', f: identity },
     { header: 'LPA', prop: "lpa", type: 'currency', f: identity },
-    { header: 'Dividendos', prop: "dividendos", type: 'currency', f: getMinYearDividends },
-    { header: 'Quantidade', type: 'number', f:  computeStockQuantity},
-    { header: 'Valor Corrente', type: 'currency', f: currentValue },
-    { header: 'Preço Alvo Bazin', type: 'currency', f: precoAlvoBazin },
-    { header: 'Preço Alvo Graham', type: 'currency', f: precoAlvoGraham },
+    { header: 'Dividendos', prop: "dividendos", type: 'currency', f: computeDividends },
+    { header: 'Quantidade', type: 'number', f: computeStockQuantity },
+    { header: 'Valor Corrente', type: 'currency', f: computeCurrentValue },
+    { header: 'Preço Alvo Bazin', type: 'currency', f: computePrecoAlvoBazin },
+    { header: 'Preço Alvo Graham', type: 'currency', f: computePrecoAlvoGraham },
 ];
+let dividends = [new MinFiveYear()];
 let cFormatter = new Intl.NumberFormat('pt-br', { style: 'currency', currency: 'BRL' });
 let nFormatter = new Intl.NumberFormat('pt-br', { style: 'decimal', minimumFractionDigits: 0, maximumFractionDigits: 0 });
 async function addTicker(ticker) {
@@ -48,27 +93,35 @@ function getMinYearDividends(dividendos) {
     div.sort((a, b) => a - b);
     return div.shift();
 }
+function computeDividends(dividendos) {
+    let index = parseInt(document.querySelector("#dividendos").value);
+    let obj = dividends[index];
+    obj.setMatrix(dividendos);
+    return obj.compute();
+}
 function computeStockQuantity(obj) {
     let salary = document.querySelector("#salario").valueAsNumber;
-    let dividendos = getMinYearDividends(obj.dividendos);
+    let dividendos = computeDividends(obj.dividendos);
     return 12 * salary / (dividendos === 0 ? 0.01 : dividendos);
 }
-function currentValue(obj) {
+function computeCurrentValue(obj) {
     let preco = obj.preco;
     let quantidade = computeStockQuantity(obj);
     return preco * quantidade;
 }
-function precoAlvoBazin(obj) {
-    let dividendos = getMinYearDividends(obj.dividendos);
+function computePrecoAlvoBazin(obj) {
+    let dividendos = computeDividends(obj.dividendos);
     let rentabilidade = document.querySelector("#rentabilidade").valueAsNumber;
     return 100 * dividendos / rentabilidade;
 }
-function precoAlvoGraham(obj) {
+function computePrecoAlvoGraham(obj) {
     let pl = document.querySelector("#pl").valueAsNumber;
     let pa = document.querySelector("#pa").valueAsNumber;
     return Math.sqrt(pl * pa * obj.vpa * obj.lpa);
 }
 onload = () => {
+    let select = document.querySelector("#dividendos");
+    dividends.forEach((v, i) => select.add(new Option(v.toString(), i)));
     let thead = document.querySelector("thead");
     let tr = thead.insertRow(-1);
     columns.forEach(obj => {
