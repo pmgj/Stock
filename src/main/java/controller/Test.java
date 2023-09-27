aa84b7dfcebd124e762e9c52694f720371582536
Paulo Gonçalves <paulogoncalves@recife.ifpe.edu.br>
2023-09-27 10:55:11 -0300

    Better selector

--- src/main/java/controller/Test.java
+++ src/main/java/controller/Test.java
@@ -12,7 +12,7 @@ import org.jsoup.select.Elements;
 import jakarta.json.bind.JsonbBuilder;
 
 public class Test {
-    public static void main3(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         String ticker = "mxrf11";
         String url = String.format("https://statusinvest.com.br/fundos-imobiliarios/%s", ticker);
         Document doc = Jsoup.connect(url).get();
@@ -21,7 +21,7 @@ public class Test {
                 "#main-2 > div.container.pb-7 > div.top-info.d-flex.flex-wrap.justify-between.mb-3.mb-md-5 > div.info.special.w-100.w-md-33.w-lg-20 > div > div:nth-child(1) > strong");
         Double pvp = extract.apply(
                 "#main-2 > div.container.pb-7 > div:nth-child(5) > div > div:nth-child(2) > div > div:nth-child(1) > strong");
-        Elements div = doc.select("#main-2 > div.container.pb-7 > div:nth-child(8) > #earning-section > #results");
+        Elements div = doc.select("#earning-section #results");
         String dl = div.attr("value");
         List<Dividends> dividends = JsonbBuilder.create().fromJson(dl, new ArrayList<Dividends>() {
         }.getClass().getGenericSuperclass());
@@ -29,7 +29,7 @@ public class Test {
         System.out.println(JsonbBuilder.create().toJson(out));
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main2(String[] args) throws IOException {
         String ticker = "sanb11";
         String url = String.format("https://statusinvest.com.br/acoes/%s", ticker);
         Document doc = Jsoup.connect(url).get();
